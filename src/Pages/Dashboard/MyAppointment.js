23de76a907b599aba8fcdd34066be3e3876f87a9
Anindita Roy <anindita.roy.mu@gmail.com>
2022-05-25 20:45:18 +0600

    user admin code added

--- src/Pages/Dashboard/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment.js
@@ -34,8 +34,8 @@ const MyAppointment = () => {
     return (
         <div>
             <h2>My appointments: {appointments.length}</h2>
-            <div class="overflow-x-auto">
-                <table class="table w-full">
+            <div className="overflow-x-auto">
+                <table className="table w-full">
                     <thead>
                         <tr>
                             <th></th>
