a1965cf7d4d59d2dd206b15718957890b66304f8
Anindita Roy <anindita.roy.mu@gmail.com>
2022-05-26 08:27:04 +0600

    stripe payment success code added

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -6,8 +6,9 @@ const CheckoutForm = ({ appointment }) => {
     const stripe = useStripe();
     const elements = useElements();
     const [cardError, setCardError] = useState('');
+    const [success, setSuccess] = useState('');
     const [clientSecret, setClientSecret] = useState('');
-    const { price } = appointment;
+    const { price, patient, patientName } = appointment;
     useEffect(() => {
         fetch('http://localhost:5000/create-payment-intent', {
             method: 'POST',
@@ -15,30 +16,56 @@ const CheckoutForm = ({ appointment }) => {
                 'content-type': 'application/json',
                 'authorization': `Bearer ${localStorage.getItem('accessToken')}`
             },
-            body: JSON.stringify(price)
+            body: JSON.stringify({ price })
         })
             .then(res => res.json())
             .then(data => {
                 if (data?.clientSecret) {
-                    setClientSecret(data.clientSecret)
+                    setClientSecret(data.clientSecret);
                 }
-            })
+            });
     }, [price])
     const handleSubmit = async (event) => {
         event.preventDefault();
+
         if (!stripe || !elements) {
             return;
         }
+
         const card = elements.getElement(CardElement);
+
         if (card === null) {
             return;
         }
+
         const { error, paymentMethod } = await stripe.createPaymentMethod({
             type: 'card',
             card
-        })
+        });
 
-        console.log(error?.message || '');
+        setCardError(error?.message || '')
+        setSuccess('');
+        // confirm card payment
+        const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: patientName,
+                        email: patient
+                    },
+                },
+            },
+        );
+        if (intentError) {
+            setCardError(intentError?.message);
+        }
+        else {
+            setCardError('');
+            console.log(paymentIntent);
+            setSuccess('Congrats! Your payment is completed.')
+        }
 
     }
     return (
@@ -60,12 +87,15 @@ const CheckoutForm = ({ appointment }) => {
                         },
                     }}
                 />
-                <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe || !{ clientSecret }}>
+                <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe || !clientSecret}>
                     Pay
                 </button>
             </form>
             {
                 cardError && <p className='text-red-500'>{cardError}</p>
+            }
+            {
+                success && <p className='text-green-500'>{success}</p>
             }
         </>
     );
--- src/Pages/Dashboard/DoctorsRow.js
+++ src/Pages/Dashboard/DoctorsRow.js
@@ -23,15 +23,15 @@ const DoctorsRow = ({ doctor, index, refetch, setDeletingDoctor }) => {
     return (
         <tr>
             <th>{index + 1}</th>
-            <td><div class="avatar">
-                <div class="w-16 rounded-full">
+            <td><div className="avatar">
+                <div className="w-16 rounded-full">
                     <img src={img} alt={name} />
                 </div>
             </div></td>
             <td>{name}</td>
             <td>{specialty}</td>
             <td>
-                <label onClick={() => setDeletingDoctor(doctor)} for="delete-confirm-modal" class="btn  btn-xs btn-error">Delete</label>
+                <label onClick={() => setDeletingDoctor(doctor)} for="delete-confirm-modal" className="btn  btn-xs btn-error">Delete</label>
             </td>
         </tr>
     );
